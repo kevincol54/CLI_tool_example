b933b97704b4924f818d3121daccca0f75243132
Kevin Collins <kevincol54@gmail.com>
2015-02-16 21:15:25 -0500

    Revert "epic commit. pass 'cats' as symbol to learn the meaning of life"

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var request = require('superagent'),
-    chalk   = require('chalk');
+var request = require('superagent');
 
 
 function ask(question, callback) {
@@ -16,11 +15,7 @@ function ask(question, callback) {
 
 ask("What is the symbol of the stock you would like to look up?", function(symbol) {
   console.log("Your symbol is: ", symbol);
-  if (symbol == "CATS"){
-    getCats()
-  } else {
-    getQuote(symbol)
-  }
+  getQuote(symbol)
 });
 
 function getQuote (symbol) {
@@ -31,20 +26,5 @@ function getQuote (symbol) {
     .end(function(error, res){
       stock = JSON.parse(res.text);
       console.log("The last price of " + stock.Name + " was $" + stock.LastPrice);
-    });
-}
-
-function getCats () {
-  var query = {v: '1.0', rsz: '8', q: "nic cage cats", safe: 'active'}
-  request
-    .get('http://ajax.googleapis.com/ajax/services/search/images')
-    .query(query)
-    .end(function(error, res){
-      var images = JSON.parse(res["text"])
-      images = images.responseData.results
-      if (images.length > 0) {
-        var image  = images[Math.floor(Math.random() * images.length)]
-        console.log(chalk.yellow("Clickbait:"), image.unescapedUrl)
-      }
     });
 }
